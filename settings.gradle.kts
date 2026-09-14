e3a82b4136ec4018ffb89c761a618b85466f771a
Du Jingtao_yoga <dujingtaoyoga@2ygwql.onmicrosoft.com>
2026-09-15 06:30:25 +0800

    fix: add aliyun mirror for faster AGP gradle plugin sync in China

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,8 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -14,6 +10,8 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
